69937ffd71f948256639c53b7024414d0d496751
Антон Макаров <loheee2323123123@gmail.com>
2026-04-24 15:59:24 +0300

    Porotype 2: add flap mechanism;

--- trafficLight.App/Controllers/TrafficLightController.cs
+++ trafficLight.App/Controllers/TrafficLightController.cs
@@ -1,3 +1,4 @@
+using trafficLight.App.Models;
 using trafficLight.App.Services;
 
 namespace trafficLight.App.Controllers;
@@ -7,6 +8,7 @@ public class TrafficLightController
     private readonly TrafficLight _cowLight;
     private readonly TrafficLight _sheepLight;
     private readonly List<TrafficLightPhase> _phases;
+    private bool _flapIsOpen;
 
     public TrafficLightController(
         TrafficLight cowLight, 
@@ -18,14 +20,38 @@ public class TrafficLightController
         _phases = phases;
     }
 
+    private void UpdateFlap(TrafficLightColor color)
+    {
+        switch (color)
+        {
+            case TrafficLightColor.Green:
+                _flapIsOpen = true;
+                break;
+            case TrafficLightColor.Red:
+                _flapIsOpen = false;
+                break;
+            case TrafficLightColor.Yellow:
+                break;
+        }
+    }
+    
+    private string GetFlapSymbol()
+    {
+        return _flapIsOpen ? "О" : "З";
+    }
+
     public async Task StartAsync()
     {
         while (true)
         {
             foreach (var phase in _phases)
             {
+                UpdateFlap(phase.CowColor);
+                
                 _cowLight.SetColor(phase.CowColor);
                 _sheepLight.SetColor(phase.SheepColor);
+                
+                Console.WriteLine($"Flap: {GetFlapSymbol()}");
                 
                 await Task.Delay(phase.TimeOfLight * 1000);
             }
